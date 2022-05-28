4e4c86811cb37615fb1407c0ec00bdb374778fb0
Rea Tabofunda <reacarolyn@gmail.com>
2022-05-27 17:56:53 -0700

    Login as teacher

--- src/test/java/StepDefs/MyStepdefs.java
+++ src/test/java/StepDefs/MyStepdefs.java
@@ -1,10 +1,48 @@
 package StepDefs;
 
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class MyStepdefs {
     @cucumber.api.java.en.Given("I login to ASK Page as Teacher")
     public void iLoginToASKPageAsTeacher() {
         getDriver().get("http://ask-m.portnov.com/#/login");
+    }
+
+    @Then("^I should see page title as \"([^\"]*)\"$")
+    public void iShouldSeePageTitleAs(String title) {
+        assertThat(getDriver().getTitle()).isEqualTo(title);
+    }
+
+    @Then("^I click on element with xpath \"([^\"]*)\"$")
+    public void iClickOnElementWithXpath(String xpath) {
+        getDriver().findElement(By.xpath(xpath)).click();
+    }
+
+    @When("^I type \"([^\"]*)\" into element with xpath \"([^\"]*)\"$")
+    public void iTypeIntoElementWithXpath(String text, String xpath) {
+        getDriver().findElement(By.xpath(xpath)).sendKeys(text);
+    }
+
+    @Then("^I click on element using JavaScript with xpath \"([^\"]*)\"$")
+    public void iClickOnElementUsingJavaScriptWithXpath(String xpath) {
+        WebElement element = getDriver().findElement(By.xpath(xpath));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+    }
+
+    @Then("^I wait for element with xpath \"([^\"]*)\" to be present$")
+    public void iWaitForElementWithXpathToBePresent(String xpath) {
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
     }
 }
--- src/test/resources/login.feature
+++ src/test/resources/login.feature
@@ -4,3 +4,8 @@
     @login1
       Scenario: Login to ASK
       Given I login to ASK Page as Teacher
+      Then I should see page title as "Assessment Control @ Portnov"
+      When I type "reacarolyn@gmail.com" into element with xpath "//input[@formcontrolname='email']"
+      And I type "12345" into element with xpath "//input[@formcontrolname='password']"
+      Then I click on element using JavaScript with xpath "//button[@type='submit']"
+      And I wait for element with xpath "//*[text()='TEACHER']" to be present
