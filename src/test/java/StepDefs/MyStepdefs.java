67b62865acffe935587a912681c482638689f6ad
Tanuja <tanujakharche26@gmail.com>
2022-05-31 16:21:28 -0700

    Updated Login scenario : Removed hard code and implemented proper wait conditions

--- src/test/java/StepDefs/MyStepdefs.java
+++ src/test/java/StepDefs/MyStepdefs.java
@@ -1,39 +1,48 @@
 package StepDefs;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
-
-import static java.lang.Thread.sleep;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static support.TestContext.getDriver;
 
 public class MyStepdefs {
-    @cucumber.api.java.en.Given("I login to ASK Page as Teacher")
-    public void iLoginToASKPageAsTeacher() throws InterruptedException {
-        getDriver().get("http://ask-m.portnov.com/#/login");
-        getDriver().findElement(By.xpath("//input[@id='mat-input-0']")).click();
-        getDriver().findElement(By.xpath("//input[@id='mat-input-0']")).sendKeys("larry211@bergservices.cf");
-        getDriver().findElement(By.xpath("//input[@id='mat-input-1']")).click();
-        getDriver().findElement(By.xpath("//input[@id='mat-input-1']")).sendKeys("user1234");
-        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(1000);
-        getDriver().findElement(By.xpath("//h5[contains(text(),\"User's Management\")]")).click();
-        Thread.sleep(1000);
-        Boolean user1 = getDriver().findElement(By.cssSelector("mat-card")).isDisplayed();
-        assertThat(user1).isTrue();
+
+    @Given("I open ASK page using url as {string}")
+    public void iOpenASKPageUsingUrlAs(String url) {
+        getDriver().get(url);
     }
 
-    @Given("I login to ASK Page as Student")
-    public void iLoginToASKPageAsStudent() throws InterruptedException {
-        getDriver().get("http://ask-m.portnov.com/#/login");
-        getDriver().findElement(By.xpath("//input[@id='mat-input-0']")).click();
-        getDriver().findElement(By.xpath("//input[@id='mat-input-0']")).sendKeys("jasonjason0@omdiaco.com");
-        getDriver().findElement(By.xpath("//input[@id='mat-input-1']")).click();
-        getDriver().findElement(By.xpath("//input[@id='mat-input-1']")).sendKeys("user1234");
-        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
-        Thread.sleep(1000);
-        getDriver().findElement(By.xpath("//h5[contains(text(),'My Grades')]")).click();
-        Thread.sleep(1000);
+    @When("I enter email as {string} and password as {string} and click Sign In")
+    public void iEnterEmailAsAndPasswordAsAndClickSignIn(String str1, String str2) {
+       WebElement email= getDriver().findElement(By.xpath("//input[@id='mat-input-0']"));
+       email.sendKeys(str1);
+       WebElement password = getDriver().findElement(By.xpath("//input[@id='mat-input-1']"));
+       password.sendKeys(str2);
+       WebElement signIn = getDriver().findElement(By.xpath("//button[@type='submit']"));
+       signIn.click();
+    }
+
+    @And("I click on Users Management from Navigation menu")
+    public void iClickOnUsersManagementFromNavigationMenu() {
+        WebDriverWait wait = new WebDriverWait(getDriver(),10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[contains(text(),'Users Management')]")));
+        WebElement userManage = getDriver().findElement(By.xpath("//h5[contains(text(),'Users Management')]"));
+        userManage.click();
+        Boolean grades1 = getDriver().findElement(By.cssSelector("mat-card")).isDisplayed();
+        assertThat(grades1).isTrue();
+    }
+
+    @And("I click on Grades from Navigation menu")
+    public void iClickOnGradesFromNavigationMenu() {
+        WebDriverWait wait1 = new WebDriverWait(getDriver(),10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[contains(text(),'My Grades')]")));
+        WebElement grades = getDriver().findElement(By.xpath("//h5[contains(text(),'My Grades')]"));
+        grades.click();
         Boolean grades1 = getDriver().findElement(By.cssSelector("mat-card")).isDisplayed();
         assertThat(grades1).isTrue();
     }
--- src/test/resources/login.feature
+++ src/test/resources/login.feature
@@ -2,9 +2,13 @@
 Feature: Login Page
 
   @login1
-  Scenario: Login to ASK as Teacher
-    Given I login to ASK Page as Teacher
+  Scenario: Login to ASK as Teacher account
+    Given I open ASK page using url as "http://ask-m.portnov.com/#/login"
+    When I enter email as "larry211@bergservices.cf" and password as "user1234" and click Sign In
+    And I click on Users Management from Navigation menu
 
-  @login2
-  Scenario: Login to ASK as Student
-    Given I login to ASK Page as Student
+   @login2
+   Scenario: Login to ASK as Student account
+     Given I open ASK page using url as "http://ask-m.portnov.com/#/login"
+     When I enter email as "jasonjason0@omdiaco.com" and password as "user1234" and click Sign In
+     And I click on Grades from Navigation menu
